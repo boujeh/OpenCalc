beded4ed04ebf3e649fbf25961d668ff4fe943ea
mohamed <mohamed.boujeh@esprit.tn>
2012-08-02 14:44:13 +0200

    fonction somme

--- main.c
+++ main.c
@@ -2,8 +2,13 @@
 
 int main(int argc, char **argv){
 
-printf (" \n Welcome to OpenCalc \n "); 
-
+printf (" \n Welcome to OpenCalc \n ");
+int somme (int a,int b);
+{
+int s;
+s=a+b;
+return s;
+}
 
 
 return 0 ;
